f3303443de4ae4d1c6314010b48a94eec96dfc94
Daniel.Newbury <Daniel.Newbury@Shopify.com>
2017-09-04 23:49:35 -0400

    Refector ActionCable for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # config.action_cable.disable_request_forgery_protection = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # mount ActionCable.server => '/cable'
 
   root :to => redirect('/status')
 
